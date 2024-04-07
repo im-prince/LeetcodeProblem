a6ee2f5c2242e3dc0785aaffb0322dc3333f2feb
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2024-04-07 14:35:40 +0530

    commit

---
+++ src/Array/HowManyNumbersAreSmallerThanTheCurrentNumber_1365.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+
+public class HowManyNumbersAreSmallerThanTheCurrentNumber_1365 {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,6,2,7,};
+        int[] an = smallerNumbersThanCurrent(arr);
+        System.out.println(Arrays.toString(an));
+    }
+    static int[] smallerNumbersThanCurrent(int[] num) {
+        int[] ans = new int[num.length];
+        for (int i = 0; i < num.length; i++) {
+            int count = 0;
+            for (int j = 0; j < num.length; j++) {
+                if (num[i] > num[j]){
+                    count ++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
