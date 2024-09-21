cf20551c48092bc3278e1a41d17b548d77733fd2
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2024-09-21 23:58:51 +0530

    commit

---
+++ src/LinkedList/MiddleOfTheLinkedList_876.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class MiddleOfTheLinkedList_876 {
+    class Node {
+        int node;
+        ReverseLinkedList_206.Node next;
+
+        public Node(int node) {
+            this.node = node;
+        }
+
+        public Node(int node, ReverseLinkedList_206.Node next) {
+            this.node = node;
+            this.next = next;
+        }
+    }
+
+    public Node middleNode(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+        while (fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }
+
+}
