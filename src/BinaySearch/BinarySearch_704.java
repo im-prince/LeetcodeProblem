090f32a6b0508536cdb1aa40642174f0a653fa1e
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2024-09-26 23:36:33 +0530

    commit

--- src/BinaySearch/BinarySearch_704.java
+++ src/BinaySearch/BinarySearch_704.java
@@ -3,10 +3,10 @@ package BinaySearch;
 // https://leetcode.com/problems/binary-search/description/
 public class BinarySearch_704 {
     public static void main(String[] args) {
-        int [] arr={-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
-        int target=22;
-        int ans=search(arr,target);
-        System.out.println(ans);
+//        int [] arr={-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
+//        int target=22;
+//        int ans=search(arr,target);
+//        System.out.println(ans);
     }
 
     static int search(int[] nums, int target) {
