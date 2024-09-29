368fa8c98bde2b8780852eb7f44716090ee0ee44
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2024-09-29 22:48:03 +0530

    commit

--- src/Array/ShuffleTheArray_1470.java
+++ src/Array/ShuffleTheArray_1470.java
@@ -7,7 +7,9 @@ public class ShuffleTheArray_1470 {
         int[] nums = {2,5,1,3,4,7};
         int n = 3;
         System.out.println(Arrays.toString(shuffle(nums,n)));
+
     }
+
     static int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2*n];
         int i=0, j=n;
