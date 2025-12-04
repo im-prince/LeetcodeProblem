0485634cd9746b0511491492a33fba6f8a5aaef5
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2025-12-04 15:59:26 +0530

    commit

--- src/Array/RotateArray_189.java
+++ src/Array/RotateArray_189.java
@@ -1,25 +1,48 @@
 package Array;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class RotateArray_189 {
 
-    static void rotate(int[] num, int k) {
-        if(k==0){
-            return;
-        }
-        if (num== null || num.length==0){
-            return;
-        }
-        int[] ans = new int[num.length];
-        for (int i = 0; i < num.length; i++) {
-            int newIndex = (i+k)% num.length;
-            ans[newIndex] = num[i];
-        }
-        // assign back to original array
-        for(int i = 0; i < num.length; i++) {
-            num[i] = ans[i];
-        }
 
+    public static void main(String[] args) {
+        int []num = {1, 2, 3, 4, 5, 6, 7};
+
+        rotate(num,3);
+        printArray(num);
+    }
+    static void printArray(int [] num){
+        for(int nums:num){
+            System.out.print(nums+ " ");
+        }
+    }
+
+//    static void rotate(int[] num, int k) {
+//        if(k==0){
+//            return;
+//        }
+//        if (num== null || num.length==0){
+//            return;
+//        }
+//        int[] ans = new int[num.length];
+//        for (int i = 0; i < num.length; i++) {
+//            int newIndex = (i+k)% num.length;
+//            ans[newIndex] = num[i];
+//        }
+//        // assign back to original array
+//        for(int i = 0; i < num.length; i++) {
+//            num[i] = ans[i];
+//        }
+
+
+    static void rotate(int[] num, int k) {
+        int n = num.length;
+        for (int i = 0; i <=k; i++) {
+            int temp = num[0];
+            for (int j = 0; j < n-1; j++) {
+                num[j] = num[j+1];
+            }
+            num[n-1] = temp;
+        }
     }
 }
